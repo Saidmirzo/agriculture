a18190abef5f329fdac67e7e21ee39582e1f2d54
Saidmirzo <saidmirzobahromov88@gmail.com>
2025-02-11 14:54:53 +0500

    Edit

--- core/settings.py
+++ core/settings.py
@@ -125,6 +125,9 @@ CSRF_TRUSTED_ORIGINS = [
     "https://agriculture-production-e531.up.railway.app",
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
