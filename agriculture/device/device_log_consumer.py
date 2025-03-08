48cc01641a6054c20c5681a750a36e8341565652
Saidmirzo <saidmirzobahromov88@gmail.com>
2025-03-08 08:02:55 +0500

    Fixed

--- agriculture/device/device_log_consumer.py
+++ agriculture/device/device_log_consumer.py
@@ -6,12 +6,12 @@ class DeviceLogConsumer(AsyncWebsocketConsumer):
         """Connect to WebSocket and join group"""
         await self.channel_layer.group_add("device_logs_group", self.channel_name)
         await self.accept()
-        print("✅ WebSocket Connected")
+        print("[INFO] WebSocket Connected")
 
     async def disconnect(self, close_code):
         """Leave WebSocket group on disconnect"""
         await self.channel_layer.group_discard("device_logs_group", self.channel_name)
-        print("❌ WebSocket Disconnected")
+        print("[INFO] WebSocket Disconnected")
 
     async def send_data(self, event):
         """Send real-time data updates to frontend"""
--- core/settings.py
+++ core/settings.py
@@ -51,6 +51,8 @@ STATICFILES_DIRS = [
     '/app/public',
 ]
 
+STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Directory where 'collectstatic' puts files
 
 
@@ -180,7 +182,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
